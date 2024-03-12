4ffe33dec1b5225656afe90a739505820bce9233
David Barreto <david11barreto11@gmail.com>
2024-03-12 01:12:23 -0500

    12-03-2024

--- ventas/urls.py
+++ ventas/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.PageInicio,name="PageInicioLink"),
     path('productos/',views.PageProductos,name="PageProductosLink"),
+    path('productos-Categorias/',views.PageProductosCategorias,name="PageProductosCategoriasLink"),
+    path('productos-Marcas/',views.PageProductosMarcas,name="PageProductosMarcasLink"),
+    path('productos-Proveedores/',views.PageProductosProveedores,name="PageProductosProveedoresLink"),
 ]
+
--- ventas/views.py
+++ ventas/views.py
@@ -9,9 +9,28 @@ def PageInicio(request):
 
 def PageProductos(request):
     productos = Producto.objects.all()
-    
     contex={
         'productos':productos,
     }
-    
     return render(request,"productos.html",contex)
+
+def PageProductosCategorias(request):
+    categorias = Categoria.objects.all()
+    contex={
+        'categorias':categorias,
+    }
+    return render(request,"productos_categorias.html",contex)
+
+def PageProductosMarcas(request):
+    marcas = Marca.objects.all()
+    contex={
+        'marcas':marcas,
+    }
+    return render(request,"productos_marcas.html",contex)
+
+def PageProductosProveedores(request):
+    proveedores = Proveedor.objects.all()
+    contex={
+        'proveedores':proveedores,
+    }
+    return render(request,"productos_proveedores.html",contex)
